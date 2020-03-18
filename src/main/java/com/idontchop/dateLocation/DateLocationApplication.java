8cdb25db5743e4d44e08389a14f908188f54ed7f
Nate <nate@idontchop.com>
2020-03-17 22:56:31 -0700

    Initial Commit

---
+++ src/main/java/com/idontchop/dateLocation/DateLocationApplication.java
@@ -0,0 +1,13 @@
+package com.idontchop.dateLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idontchop/dateLocation/DateLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idontchop.dateLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
