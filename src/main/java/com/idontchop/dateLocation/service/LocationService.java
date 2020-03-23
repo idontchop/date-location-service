f1d297ee5b8b52d15cfaca7f9b5c66ce6d3d5859
Nate D <nate@idontchop.com>
2020-03-23 16:52:08 -0700

    comments

--- src/main/java/com/idontchop/dateLocation/service/LocationService.java
+++ src/main/java/com/idontchop/dateLocation/service/LocationService.java
@@ -354,10 +354,12 @@ public class LocationService {
 	
 	public LocationPage getLocationsNear ( String latArg, String lngArg, int km, List<String> types, int page) {
 		
+		// setup NearQuery
 		NearQuery nearQuery = buildNearQuery(latArg, lngArg, km).skip(page * PAGESIZE).limit(PAGESIZE);
 		nearQuery.inKilometers();
-		nearQuery.query(buildQuery(null,types));
+		nearQuery.query(buildQuery(null,types)); // add regular query with username / types criteria
 				
+		// Do Near Query
 		GeoResults<Location> results = mongoTemplate.geoNear(nearQuery, Location.class);
 		
 		// Total results needed for page count
