fab6cf7c38e252345a84eff5406229f5836ba587
Nate <nate@idontchop.com>
2020-04-09 23:26:46 -0700

    comment

--- src/main/java/com/idontchop/dateLocation/service/LocationService.java
+++ src/main/java/com/idontchop/dateLocation/service/LocationService.java
@@ -356,13 +356,12 @@ public class LocationService {
 		
 		// setup NearQuery
 		NearQuery nearQuery = buildNearQuery(latArg, lngArg, km).skip(page * PAGESIZE).limit(PAGESIZE);
-<<<<<<< HEAD
+
 		// nearQuery.inKilometers(); ? don't need since we convert to radius
 		nearQuery.query(buildQuery(null,types));
-=======
+
 		nearQuery.inKilometers();
 		nearQuery.query(buildQuery(null,types)); // add regular query with username / types criteria
->>>>>>> branch 'master' of https://github.com/idontchop/date-location-service.git
 				
 		// Do Near Query
 		GeoResults<Location> results = mongoTemplate.geoNear(nearQuery, Location.class);
