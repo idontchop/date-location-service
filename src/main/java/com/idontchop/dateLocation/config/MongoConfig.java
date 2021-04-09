7b1dfd2091f0a7d9f005716d468ace42f3cd6608
Nate <nate@idontchop.com>
2021-04-08 22:18:15 -0700

    3

--- src/main/java/com/idontchop/dateLocation/config/MongoConfig.java
+++ src/main/java/com/idontchop/dateLocation/config/MongoConfig.java
@@ -3,14 +3,17 @@ package com.idontchop.dateLocation.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 
 @Configuration
+@EnableReactiveMongoRepositories
 public class MongoConfig {
 	
 	/*
@@ -33,4 +36,10 @@ public class MongoConfig {
     public MongoTemplate mongoTemplate() throws Exception {
         return new MongoTemplate(mongo(), "test-loc");
     }
+
+	protected String getDatabaseName() {
+		return "test-loc";
+	}
+
+
 }
